8ace38e7b4f23a39a6c744478c703d65218d88ac
aman hussain <aman30dbg@gmail.com>
2026-01-08 23:03:13 +0530

    Data Validation completed

--- House_Price/constant/training_pipeline/__init__.py
+++ House_Price/constant/training_pipeline/__init__.py
@@ -14,6 +14,8 @@ FILE_NAME: str = "house_price.csv"
 
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
+
+SCHEMA_FILE_PATH=os.path.join("data_schema","schema.yaml")
 """
 Data Ingested related constant start with DATA_INGESTED VAR NAME
 """
@@ -24,3 +26,12 @@ DATA_INGESTED_DIR_NAME: str = "data_ingested"
 DATA_INGESTED_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTED_INGESTED_DIR: str ="ingested"
 DATA_INGESTED_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+"""
+Data validation related constant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yml"
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from House_Price.components.data_ingestion import DataIngestion
+from House_Price.components.data_validation import DataValidation
 from House_Price.exception.exception import HousePricePredictionException
 from House_Price.logging.logger import logging
-from House_Price.entity.config_entity import DataIngestionConfig
+from House_Price.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from House_Price.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -12,7 +13,16 @@ if __name__=="__main__":
         data_ingestion=DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
         dataingestedartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiated completed")
         print(dataingestedartifact)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestedartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validate()
+        logging.info("Data Validation Completed")
+        print(data_validation_artifact)
+
+        
         
     except Exception as e:
         raise HousePricePredictionException(e,sys)    
